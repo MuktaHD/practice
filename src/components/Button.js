d264dcfb0223883df773bfd8f23c61b1272487a2
MuktaHD <muktabhosale3@gmail.com>
2024-07-18 12:20:41 +0530

    Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+
+import '../App.css'
+function Button(props){
+    console.log(props)
+return(
+    <>
+    <button className="BTN">{props.data}</button>
+    </>
+  );
+
+
+}
+
+export default Button
